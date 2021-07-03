cb7e68c5ac639ea11285ce576896a825806fe05c
zhouhonggang <851308369@qq.com>
2021-07-03 17:53:57 +0800

    增加依赖

---
+++ src/main/java/xyz/zhhg/mydomain/entity/MydomainDB.java
@@ -0,0 +1,12 @@
+package xyz.zhhg.mydomain.entity;
+import lombok.Data;
+
+/**
+ * @author 周泓钢
+ * @className MydomainDB
+ * @date 2021/7/3
+ */
+@Data
+public class MydomainDB {
+    private String ip;
+}
